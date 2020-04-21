8489f7db40b3851ec4e4212784b412a132e062cd
pavlo <pavlo333vu@gmail.com>
2020-04-21 09:58:31 +0300

    second

---
+++ Lab02_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab02_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|  X:  | Y=f(X): |");
+            double a = 1, b = 6, dx = 0.2, y,x=a;
+            while(x<=b)
+            {
+                y = Math.Log10(x);
+                y = Math.Round(y, 4);
+                Console.WriteLine("| {0,-4} | {1,-7} |",x,y);
+                x += dx;
+                x = Math.Round(x, 1);
+            }
+        }
+    }
+}
